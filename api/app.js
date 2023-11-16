cf346c5f43330e75c308dbd97dbe17e592866f8e
Wily Tatow <witly136@gmail.com>
2023-11-16 13:13:24 -0500

    klé.

--- api/app.js
+++ api/app.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
+  //jjxjxjx
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
